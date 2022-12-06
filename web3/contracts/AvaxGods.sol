5bc61fdc6af51e116dab586db21aa05ad1c7d538
paschal533 <okwuosahpaschal@gmail.com>
2022-12-06 21:01:23 +0530

    initial commit

--- web3/contracts/AvaxGods.sol
+++ web3/contracts/AvaxGods.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.16;
 
 import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
-import '@openzeppelin/contracts/access/Ownable.sol';
+//import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol';
 
 /// @title AVAXGods
