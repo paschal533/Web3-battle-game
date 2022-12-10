23f27202cce346c373b54361e6166e5de3ff5f79
paschal533 <okwuosahpaschal@gmail.com>
2022-12-10 23:11:24 +0530

    initial commit

--- web3/contracts/AvaxGods.sol
+++ web3/contracts/AvaxGods.sol
@@ -204,7 +204,7 @@ contract AVAXGods is ERC1155, Ownable, ERC1155Supply {
   /// @param _name game token name; set by player
   function createRandomGameToken(string memory _name) public {
     require(!getPlayer(msg.sender).inBattle, "Player is in a battle"); // Require that player is not already in a battle
-    //require(isPlayer(msg.sender), "Please Register Player First"); // Require that the player is registered
+    require(isPlayer(msg.sender), "Please Register Player First"); // Require that the player is registered
     
     _createGameToken(_name); // Creates game token
   }
